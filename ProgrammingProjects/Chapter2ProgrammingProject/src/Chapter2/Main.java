5cf90e5fbeeec266b251301b392213b6e6310e84
ml <ml@mike-server.mikelimpus.com>
2018-09-11 14:17:01 -0700

    hi

---
+++ ProgrammingProjects/Chapter2ProgrammingProject/src/Chapter2/Main.java
@@ -0,0 +1,20 @@
+/**
+ * Jonathan Limpus
+ * Chapter 2 Programming Project
+ * Write a program that calculates & outputs the average of integers 1, 7, 9, and 34
+ * */
+
+package Chapter2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Variable definitions
+        int a = 1, b = 7, c = 9, d = 34;
+        int answer;
+        System.out.print("The average of the numbers 1, 7, 9, 34 = ");
+        answer = (a + b + c + d) / 4;
+        System.out.print(answer);
+
+    }
+}
