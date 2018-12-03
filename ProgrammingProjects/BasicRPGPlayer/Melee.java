f78016ee74517ead1e33a412cbd953562063f5f0
MikeLimpus <mikelimpus111@gmail.com>
2018-12-02 17:37:15 -0800

    Work done at home

---
+++ ProgrammingProjects/BasicRPGPlayer/Melee.java
@@ -0,0 +1,26 @@
+/*
+ * Jonathan Limpus
+ * Basic RPG Player
+ * Melee.java
+ */
+
+public class Melee extends Player {
+    private int strength, endurance;
+
+    public Melee() {
+        super();
+        strength = 1;
+        endurance = 1;
+    }
+
+    public Melee(String playerName, int healthPoints, int strength, int endurance) {
+        super(playerName, healthPoints);
+        this.strength = strength;
+        this.endurance = endurance;
+    }
+
+    public int getStrength() {return strength;}
+    public int getEndurance() {return endurance;}
+    public void setStrength(int strength) {this.strength = strength;}
+    public void setEndurance(int endurance) {this.endurance = endurance;}
+}
---
+++ ProgrammingProjects/BasicRPGPlayer/Player.java
@@ -0,0 +1,29 @@
+/*
+ * Jonathan Limpus
+ * Basic RPG Player
+ * Player.java
+ */
+
+public abstract class Player {
+    protected String playerName;
+    protected int healthPoints;
+
+    public Player() {
+        // Set default values
+        playerName = "Name";
+        healthPoints = 10;
+    }
+
+    public Player(String playerName, int healthPoints) {
+        this.playerName = playerName;
+        this.healthPoints = healthPoints;
+    }
+
+    // Accessors and Mutators
+    public String getName() {return playerName;}
+    public int getHealth() {return healthPoints;}
+    public void setName(String playerName) {this.playerName = playerName;}
+    public void setHealth(int healthPoints) {this.healthPoints = healthPoints;}
+
+    
+}
